ccd0926f0f73bbce043a1f07ca15fd2eb7d57956
Vague-INKD-2 <matthewtiggers99@gmail.com>
2022-10-16 11:33:49 -0400

    Initial commit

---
+++ app/src/main/java/com/zybooks/the_iceburg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.the_iceburg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
