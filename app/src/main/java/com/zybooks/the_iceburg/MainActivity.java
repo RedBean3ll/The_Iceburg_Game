62c7e17026387a6c6b336b1972b34ae0e2aad6d2
kfknaut <Kfknaut@gmail.com>
2022-10-18 13:48:39 -0400

    Settings menu open

--- app/src/main/java/com/zybooks/the_iceburg/MainActivity.java
+++ app/src/main/java/com/zybooks/the_iceburg/MainActivity.java
@@ -2,6 +2,9 @@ package com.zybooks.the_iceburg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.provider.Settings;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onSettingsClick (View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/zybooks/the_iceburg/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.the_iceburg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
+    }
+}
