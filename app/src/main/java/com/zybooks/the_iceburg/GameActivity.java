4fab5f5c8b17d2ba9417b0f6924f34fb2805c714
kfknaut <Kfknaut@gmail.com>
2022-10-23 21:24:35 -0400

    Working move button

--- app/src/main/java/com/zybooks/the_iceburg/GameActivity.java
+++ app/src/main/java/com/zybooks/the_iceburg/GameActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
@@ -34,5 +38,38 @@ public class GameActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         gameView.resume();
+    }
+
+    @Override
+
+    public boolean onTouchEvent(MotionEvent event){
+        String action = "";
+
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if((x < (gameView.screenX - 30) && x > (gameView.screenX - 230)) &&
+                (y > (gameView.screenY -230)) && y < (gameView.screenY - 30)) {
+                gameView.backgroundMovement(1);
+            }
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                gameView.backgroundMovement(0);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+
+        Log.d("deez", action + " x = " + event.getX() +
+                " y = " + event.getY());
+        return true;
     }
 }
--- app/src/main/java/com/zybooks/the_iceburg/GameView.java
+++ app/src/main/java/com/zybooks/the_iceburg/GameView.java
@@ -3,6 +3,8 @@ package com.zybooks.the_iceburg;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.SurfaceView;
 
 
@@ -10,21 +12,22 @@ public class GameView extends SurfaceView implements Runnable {
 
     private Thread thread;
     private boolean isPlaying;
-    private int screenX, screenY;
+    public int screenX, screenY;
     private float screenRatioX, screenRatioY;
     private Paint paint;
     private GameBackground background1, background2;
+    private int dir;
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
 
         this.screenX = screenX;
         this.screenY = screenY;
-        screenRatioX = 1920f / screenX;
-        screenRatioY = 1080f /screenY;
+        screenRatioX = 1920 / screenX;
+        screenRatioY = 1080 /screenY;
 
-        background1 = new GameBackground(screenX,screenY, getResources());
-        background2 = new GameBackground(screenX,screenY, getResources());
+        background1 = new GameBackground(screenX ,screenY, getResources());
+        background2 = new GameBackground(screenX ,screenY, getResources());
 
         background2.x = screenX;
 
@@ -33,13 +36,30 @@ public class GameView extends SurfaceView implements Runnable {
 
     @Override
     public void run() {
-        while (isPlaying) {
-            update ();
-            draw ();
-            sleep();
+      while (isPlaying) {
+          draw ();
+          if(dir == 1) {
+              update();
+              sleep();
+          }
         }
     }
 
+    public void backgroundMovement (int direction) {
+        dir = direction;
+        if (direction == 0) {
+                background1.x -= 0 * screenRatioX; //affects screen background movement!!!
+                background2.x -= 0 * screenRatioX;
+
+                if(background1.x + background1.background.getWidth() < screenRatioX) {
+                    background1.x = screenX;
+                }
+
+                if(background2.x + background2.background.getWidth() < 0) {
+                    background2.x = screenX;
+            }
+        }
+    }
     private void update () {
         background1.x -= 10 * screenRatioX; //affects screen background movement!!!
         background2.x -= 10 * screenRatioX;
@@ -58,6 +78,15 @@ public class GameView extends SurfaceView implements Runnable {
             Canvas canvas = getHolder().lockCanvas();
             canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
             canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
+
+            //Draw everything UI and player here, icons, player
+            Drawable player = getResources().getDrawable(R.drawable.moyaifast, null);
+            player.setBounds((screenX/2) -200,((screenY/2)-200) +200,(screenX/2)+200,((screenY/2)+200) +200);
+            player.draw(canvas);
+
+            Drawable left_arrow = getResources().getDrawable(R.drawable.arrow_button, null);
+            left_arrow.setBounds((screenX) - 230,(screenY) -230, (screenX) - 30,(screenY) -30);
+            left_arrow.draw(canvas);
 
             getHolder().unlockCanvasAndPost(canvas);
         }
@@ -85,6 +114,9 @@ public class GameView extends SurfaceView implements Runnable {
             e.printStackTrace();
         }
     }
+
+
+
 
 }
 
