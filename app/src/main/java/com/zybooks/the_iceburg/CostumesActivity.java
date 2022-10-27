c4e4269a03b5b32038828d4e6fa3b4c5427443c2
ttrain00 <tjleac0485@ung.edu>
2022-10-27 11:42:09 -0400

    Created Achievements and Costumes Activities

---
+++ app/src/main/java/com/zybooks/the_iceburg/CostumesActivity.java
@@ -0,0 +1,13 @@
+package com.zybooks.the_iceburg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostumesActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.costumes_menu);
+    }
+}
--- app/src/main/java/com/zybooks/the_iceburg/MainActivity.java
+++ app/src/main/java/com/zybooks/the_iceburg/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void onGameStartClick (View view) {
         Intent start = new Intent(this, GameActivity.class);
         startActivity(start);
+    }
+
+    public void onAchievementsClick (View view) {
+        Intent achievements = new Intent(this, AchievementsActivity.class);
+        startActivity(achievements);
+    }
+
+    public void onCostumesClick (View view) {
+        Intent costumes = new Intent(this, CostumesActivity.class);
+        startActivity(costumes);
     }
 
     public void onSettingsClick (View view) {
